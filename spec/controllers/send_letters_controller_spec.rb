e4970f94bfb04d538e27cb558202f3008c34c8ca
Vladimir Railean <valdimir.r@travod.com>
2018-11-08 10:37:07 +0200

    refactoring send_letters_controller_spec

--- spec/controllers/send_letters_controller_spec.rb
+++ spec/controllers/send_letters_controller_spec.rb
@@ -2,70 +2,70 @@
 require 'spec_helper'
 
 describe SendLettersController do
+  let(:user) { FactoryBot.create(:user) }
+  let(:book) { FactoryBot.create(:book) }
+  let(:lot) { FactoryBot.create(:lot, book: book) }
+  let(:mail_count) { ActionMailer::Base.deliveries.count }
+  let(:not_existed_user_id) { '99' }
 
   before :each do
-    @user = FactoryBot.create(:user)
-    book = FactoryBot.create(:book)
-    @lot = FactoryBot.create(:lot, :book => book)
     @user_messaga = {
-        :email => 'mymail@azbuker.ru',
-        :text => 'Fuuck yo uuuu!',
-        :type => '7',
-        :lotid => @lot.id.to_s,
-        :userid => @user.id.to_s
-    }
+      email: 'mymail@azbuker.ru',
+      text: 'Fuuck yo uuuu!',
+      type: '7',
+      lotid: lot.id.to_s,
+      userid: user.id.to_s
+      }
+    mail_count
   end
 
   it "should set correct status on error" do
-    expect {
-      post :message, {:user_message => @user_messaga.merge(:userid => '99'),
-                      :format => 'js'}
-      assigns(:status_ok).should be_false # no user 99
-      response.should render_template('success')
+      post :message, {user_message: @user_messaga.merge(userid: not_existed_user_id),
+                      format: :js}
+      expect(assigns(:status_ok)).to be_falsey # no user 99
+      expect(response).to render_template('success')
 
-      post :abuse, {:user_message => @user_messaga.merge(:userid => '99'), :format => 'js'}
-      assigns(:status_ok).should be_false # no user 99
-      response.should render_template('success')
+      post :abuse, { user_message: @user_messaga.merge(userid: not_existed_user_id), format: :js }
+      expect(assigns(:status_ok)).to be_falsey # no user 99
+      expect(response).to render_template('success')
 
-      post :message, {:user_message => @user_messaga.merge(:userid => '99')}
-      response.should_not render_template('success')
-      response.should redirect_to(lot_path(@lot))
-      flash[:alert].should == I18n.t("generic_errors.sendmsg_form_failed")
-    }.to change(ActionMailer::Base.deliveries, :count).by(0)
+      post :message, { user_message: @user_messaga.merge(userid: not_existed_user_id) }
+      expect(response).to_not render_template('success')
+      expect(response).to redirect_to(lot_path(lot))
+      expect(flash[:alert]).to eq I18n.t("generic_errors.sendmsg_form_failed")
+      expect(ActionMailer::Base.deliveries.count).to eq mail_count
   end
 
   it "should send correct message" do
-    expect {
       @user_messaga.delete(:type)
-      post :message, {:user_message => @user_messaga, :format => 'js'}
-      assigns(:status_ok).should be_truthy
-      response.should render_template('success')
+      post :message, { user_message: @user_messaga, format: :js }
+      expect(assigns(:status_ok)).to be_truthy
+      expect(response).to render_template('success')
 
       mail = ActionMailer::Base.deliveries.last
 
-      mail.to.should == [@user.email]
-      mail.subject.should == I18n.t("mailer.msg_subject", :lotid => @user_messaga[:lotid])
+      expect(mail.to).to match_array [user.email]
+      expect(mail.subject).to eq I18n.t("mailer.msg_subject", lotid: @user_messaga[:lotid])
       mail.reply_to = @user_messaga[:email]
-      mail.body.should match_selector_content("p", 'Вашей книгой интересуется человек с обратным адресом')
-    }.to change(ActionMailer::Base.deliveries, :count).by(1)
+      expect(mail.body).to match_selector_content("p", 'Вашей книгой интересуется человек с обратным адресом')
+      expect(ActionMailer::Base.deliveries.count).to eq mail_count + 1
   end
 
   it "should send correct abuse" do
-    expect {
-      post :message, {:user_message => @user_messaga}
-      response.should redirect_to(lot_path(@lot))
-      flash[:notice].should == I18n.t("messages.message_sent")
+    post :message, { user_message: @user_messaga }
+    expect(response).to redirect_to(lot_path(lot))
+    expect(flash[:notice]).to eq I18n.t("messages.message_sent")
 
-      mail = ActionMailer::Base.deliveries.last
+    mail = ActionMailer::Base.deliveries.last
 
-      mail.to.should == [Azbuker::Application.config.abusemail]
+    expect(mail.to).to match_array [Azbuker::Application.config.abusemail]
 
-      mail.subject.should == I18n.t("mailer.abuse_subject",
-                                    :lotid => @user_messaga[:lotid],
-                                    :userid => @user_messaga[:userid])
+    expect(mail.subject).to eq I18n.t("mailer.abuse_subject",
+                                  lotid: @user_messaga[:lotid],
+                                  userid: @user_messaga[:userid])
 
-      mail.reply_to = @user_messaga[:email]
-      mail.body.should match_selector_content("p", 'Поступила жалоба на пользователя')
-    }.to change(ActionMailer::Base.deliveries, :count).by(1)
+    mail.reply_to = @user_messaga[:email]
+    expect(mail.body).to match_selector_content("p", 'Поступила жалоба на пользователя')
+    expect(ActionMailer::Base.deliveries.count).to eq mail_count + 1
   end
 end
