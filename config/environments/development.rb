d29eefa42d0ec4e73d6f1fdcc18eca2b41b554e7
Combos93 <combos93@mail.ru>
2019-09-29 16:45:05 +0600

    delete deprecated, write new options on envs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,14 +11,14 @@ Azbuker::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  # Log error messages when you accidentally call methods on nil.
-  config.whiny_nils = true
-
+  # Do not eager load code on boot
+  config.eager_load = false
   # Show full error reports and disable caching
   config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
 
-
+  # Raise an error on page load if there are pending migrations
+  config.active_record.migration_error = :page_load
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
 
@@ -34,11 +34,8 @@ Azbuker::Application.configure do
 
   config.log_level = :debug
 
-  # Only use best-standards-support built into browsers
-  config.action_dispatch.best_standards_support = :builtin
-
   # Do not compress assets
-  config.assets.compress = true
+  config.assets.compress = false
 
   #config.assets.digest = true
 
