35048a47bfcc1789f0fbb5f7b066634267a8ed9f
Combos93 <combos93@mail.ru>
2019-09-29 16:50:14 +0600

    new names for keys by doc

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.public_key = ENV['RECAPTCHA_SITE_KEY']
-  config.private_key = ENV['RECAPTCHA_SECRET_KEY']
+  config.site_key = ENV['RECAPTCHA_SITE_KEY']
+  config.secret_key = ENV['RECAPTCHA_SECRET_KEY']
 end
