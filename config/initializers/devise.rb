1ba2af93fef31e86b6b0bcbd3de931d6c412971a
Combos93 <combos93@mail.ru>
2019-09-29 16:51:18 +0600

    new settings and names

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,6 +15,8 @@ Devise.setup do |config|
   # available as additional gems.
   require 'devise/orm/active_record'
 
+  config.secret_key = ENV['secret_key']
+
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
   # just :email. You can configure it to use [:username, :subdomain], so for
